a2cc678d6c398bb04f78a656f5d52753cef0f9de
Wenyu Zheng <zhengwy229@gmail.com>
2023-09-01 16:29:36 +0100

    Challenge completed 58% for boilerplate-express

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,10 +1,27 @@
+require("dotenv").config();
+
 let express = require("express");
 let app = express();
 
 console.log("Hello World");
 
+app.use("/json", (req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "Hello json".toUpperCase() });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
+
+app.use("/public", express.static(__dirname + "/public"));
 
 module.exports = app;
