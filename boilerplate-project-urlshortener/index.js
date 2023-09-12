ce39095c0bb3f5d09ec87daff6443c6b2c3bb731
Wenyu Zheng <zhengwy229@gmail.com>
2023-09-12 12:53:58 +0100

    connect database

--- boilerplate-project-urlshortener/index.js
+++ boilerplate-project-urlshortener/index.js
@@ -1,24 +1,43 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// DB set up
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const urlSchema = new mongoose.Schema({
+  original: { type: String, required: true },
+  short: Number,
+});
+const Url = mongoose.model("URL", urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl", (req, res) => {
+  console.log(req);
+  res.json({
+    // original_url:,
+    // short_url:
+  });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:short_url", (req, res) => {});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
