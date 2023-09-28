6c8b90b97326b7d475f243666f971071ebf5fba6
Wenyu Zheng <zhengwy229@gmail.com>
2023-09-28 16:49:53 +0100

    Passing get url in url-shortener project

--- boilerplate-project-urlshortener/index.js
+++ boilerplate-project-urlshortener/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const validURL = require("valid-url");
+const shortID = require("shortid");
 
 // DB set up
 mongoose.connect(process.env.MONGO_URI, {
@@ -13,7 +14,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
 const urlSchema = new mongoose.Schema({
   original: { type: String, required: true },
-  short: Number,
+  short: String,
 });
 const Url = mongoose.model("URL", urlSchema);
 
@@ -28,11 +29,8 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-let count = 1;
-
 app.post("/api/shorturl", (req, res) => {
   if (!validURL.isWebUri(req.body.url)) {
-    console.log(req.body.url);
     return res.json({ error: "invalid url" });
   } else {
     Url.findOne({ original: req.body.url })
@@ -43,7 +41,7 @@ app.post("/api/shorturl", (req, res) => {
             short_url: found.short,
           });
         } else {
-          count++;
+          const count = shortID.generate();
           new Url({ original: req.body.url, short: count }).save();
           return res.json({
             original_url: req.body.url,
@@ -55,99 +53,16 @@ app.post("/api/shorturl", (req, res) => {
   }
 });
 
+app.get("/api/shorturl/:short_url?", (req, res) => {
+  Url.findOne({ short: req.params.short_url }).then((found) => {
+    if (found) {
+      return res.redirect(found.original);
+    } else {
+      return res.status(404).json("No URL found");
+    }
+  });
+});
+
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
-
-// /* eslint-disable no-unused-vars */
-// /* eslint-disable no-undef */
-// const express = require("express");
-// const mongo = require("mongodb");
-// const mongoose = require("mongoose");
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const validURL = require("valid-url");
-// const shortID = require("shortid");
-// require("dotenv").config();
-
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// // MongoDB and mongoose connect
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-// // Database schema
-// const urlSchema = new mongoose.Schema({
-//   originalURL: String,
-//   shortURL: String,
-// });
-
-// const URL = mongoose.model("URL", urlSchema);
-
-// // App middleware
-// app.use(cors());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use("/public", express.static(`${process.cwd()}/public`));
-// app.get("/", function (req, res) {
-//   res.sendFile(`${process.cwd()}/views/index.html`);
-// });
-
-// // Response for POST request
-// app.post("/api/shorturl", async (req, res) => {
-//   const url = req.body.url;
-//   const shortURL = shortID.generate();
-//   console.log(validURL.isUri(url));
-//   if (validURL.isWebUri(url) === undefined) {
-//     res.json({
-//       error: "invalid url",
-//     });
-//   } else {
-//     try {
-//       let findOne = await URL.findOne({
-//         originalURL: url,
-//       });
-//       if (findOne) {
-//         res.json({
-//           original_url: findOne.originalURL,
-//           short_url: findOne.shortURL,
-//         });
-//       } else {
-//         findOne = new URL({
-//           originalURL: url,
-//           shortURL,
-//         });
-//         await findOne.save();
-//         res.json({
-//           original_url: findOne.originalURL,
-//           short_url: findOne.shortURL,
-//         });
-//       }
-//     } catch (err) {
-//       console.log(err);
-//       res.status(500).json("Server error..");
-//     }
-//   }
-// });
-
-// // Redirect shortened URL to Original URL
-// // app.get("/api/shorturl/:shortURL?", async (req, res) => {
-// //   try {
-// //     const urlParams = await URL.findOne({
-// //       shortURL: req.params.shortURL,
-// //     });
-// //     if (urlParams) {
-// //       return res.redirect(urlParams.originalURL);
-// //     }
-// //     return res.status(404).json("No URL found");
-// //   } catch (err) {
-// //     console.log(err);
-// //     res.status(500).json("Server error..");
-// //   }
-// // });
-// // Listens for connections
-// app.listen(port, function () {
-//   console.log("Node.js listening ...");
-// });
