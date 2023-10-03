c1d4d94ce458c9c409694e91a12f3cbcc35e8786
Wenyu Zheng <zhengwy229@gmail.com>
2023-10-03 12:00:55 +0100

    pass post new exercise tests

--- boilerplate-project-exercisetracker/index.js
+++ boilerplate-project-exercisetracker/index.js
@@ -1,21 +1,22 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
 require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(cors());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+});
+
+// DB setup
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 // Models
@@ -50,88 +51,88 @@ app.get("/api/users", (req, res) => {
 
 // Post new exercise
 app.post("/api/users/:_id/exercises", (req, res) => {
-  // Exercise.create({
-  //   userId: req.body[":_id"],
-  //   description: req.body.description,
-  //   duration: req.body.duration,
-  //   date: req.body.date ? new Date(req.body.date) : new Date(),
-  // }).then((data) => {
-  //   console.log(data);
+  const userId = req.params._id;
+  const { description, duration, date } = req.body;
 
-  //   User.findById(data.userId).then((foundUser) => {
-  //     return res.json({
-  //       _id: data.userId,
-  //       username: foundUser.username,
-  //       description: data.description,
-  //       duration: data.duration,
-  //       date: data.date.toDateString(),
-  //     });
-  //   });
-  // });
+  const newObj = new Exercise({
+    userId,
+    description,
+    duration,
+    date: date ? new Date(date) : new Date(),
+  });
 
-  const date = req.body.date ? new Date(req.body.date) : new Date();
-  const duration = parseInt(req.body.duration);
+  User.findOne({ _id: userId }).then(async (user) => {
+    if (!user) return res.json({ error: "User not found" });
 
-  if (date == "Invalid Date") return res.json({ error: "Invalid Date" });
-  if (isNaN(duration)) return res.json({ error: "Duration must be a integer" });
-
-  if (!req.body[":_id"] || !req.body.description || !duration) {
-    return res.json({ error: "More info needed" });
-  }
-
-  User.findById(req.body[":_id"]).then((foundUser) => {
-    if (!foundUser) return res.json({ error: "UserId not found" });
-
-    Exercise.create({
-      userId: foundUser._id,
-      description: req.body.description,
-      duration: req.body.duration,
-      date: date,
-    });
-
+    const obj = await newObj.save();
     return res.json({
-      _id: foundUser._id,
-      username: foundUser.username,
-      description: req.body.description,
-      duration: duration,
-      date: date.toDateString(),
+      username: user.username,
+      _id: obj.userId,
+      description: obj.description,
+      duration: obj.duration,
+      date: obj.date.toDateString(),
     });
   });
+
+  // const date = req.body.date ? new Date(req.body.date) : new Date();
+  // const duration = parseInt(req.body.duration);
+  // console.log(req.body.description, req.body.duration, req.body.date);
+  // if (date == "Invalid Date") return res.json({ error: "Invalid Date" });
+  // if (isNaN(duration)) return res.json({ error: "Duration must be a integer" });
+  // if (!req.body[":_id"] || !req.body.description || !duration) {
+  //   return res.json({ error: "More info needed" });
+  // }
+  // User.findById(req.body[":_id"]).then((foundUser) => {
+  //   if (!foundUser) return res.json({ error: "UserId not found" });
+  //   Exercise.create({
+  //     userId: foundUser._id,
+  //     description: req.body.description,
+  //     duration: req.body.duration,
+  //     date: date,
+  //   });
+  //   return res.json({
+  //     _id: foundUser._id,
+  //     username: foundUser.username,
+  //     description: req.body.description,
+  //     duration: duration,
+  //     date: date.toDateString(),
+  //   });
+  // });
 });
 
 // Get exercise logs
-app.get("/api/users/:_id/logs", (req, res) => {
-  console.log(`req.body: ${JSON.stringify(req.body)}`);
-  console.log(`req.params: ${JSON.stringify(req.params)}`);
-  console.log(`req.query: ${JSON.stringify(req.query)}`);
+// app.get("/api/users/:_id/logs", (req, res) => {
+//   console.log(`req.body: ${JSON.stringify(req.body)}`);
+//   console.log(`req.params: ${JSON.stringify(req.params)}`);
+//   console.log(`req.query: ${JSON.stringify(req.query)}`);
 
-  User.findById(req.params._id)
-    .then((foundUser) => {
-      // console.log({ foundUser });
+//   User.findById(req.params._id)
+//     .then((foundUser) => {
+//       // console.log({ foundUser });
 
-      if (!foundUser) return res.json({ error: "UserId not found" });
+//       if (!foundUser) return res.json({ error: "UserId not found" });
 
-      Exercise.find({ userId: req.params._id })
-        .then((data) => {
-          // console.log(data.length, { data });
+//       Exercise.find({ userId: req.params._id })
+//         .then((data) => {
+//           // console.log(data.length, { data });
 
-          return res.json({
-            username: foundUser.username,
-            _id: foundUser._id,
-            count: data.length,
-            log: data.map((d) => {
-              return {
-                description: d.description,
-                duration: d.duration,
-                date: new Date(d.date).toDateString(),
-              };
-            }),
-          });
-        })
-        .catch((err) => console.log(err));
-    })
-    .catch((err) => console.log(err));
-});
+//           return res.json({
+//             username: foundUser.username,
+//             _id: foundUser._id,
+//             count: data.length,
+//             log: data.map((d) => {
+//               return {
+//                 description: d.description,
+//                 duration: d.duration,
+//                 date: new Date(d.date).toDateString(),
+//               };
+//             }),
+//           });
+//         })
+//         .catch((err) => console.log(err));
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
