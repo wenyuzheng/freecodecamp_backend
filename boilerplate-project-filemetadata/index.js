518d267a15864826150a51e49be3092fabb353e9
Wenyu Zheng <zhengwy229@gmail.com>
2023-10-03 18:19:50 +0100

    Install multer

--- boilerplate-project-filemetadata/index.js
+++ boilerplate-project-filemetadata/index.js
@@ -1,20 +1,20 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+const multer = require("multer");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+const upload = multer({ dest: "uploads/" });
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
