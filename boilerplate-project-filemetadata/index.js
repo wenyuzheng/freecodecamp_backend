540b3f66dbeebd21f69eebe1daf249c15c447a94
Wenyu Zheng <zhengwy229@gmail.com>
2023-10-03 18:21:27 +0100

    pass all tests

--- boilerplate-project-filemetadata/index.js
+++ boilerplate-project-filemetadata/index.js
@@ -1,20 +1,29 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+const multer = require("multer");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+const upload = multer({ dest: "uploads/" });
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
 
-
+  return res.json({
+    name: originalname,
+    type: mimetype,
+    size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
